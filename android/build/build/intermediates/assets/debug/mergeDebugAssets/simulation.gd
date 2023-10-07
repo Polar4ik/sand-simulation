692f8af9078841c3251ac6e1547460b424dbc97b
Polar4ik <atossnikitoss@gmail.com>
2023-10-07 16:21:50 +0400

    Added water, immortal walls and sand in water behaviours

--- android/build/build/intermediates/assets/debug/mergeDebugAssets/simulation.gd
+++ android/build/build/intermediates/assets/debug/mergeDebugAssets/simulation.gd
@@ -5,80 +5,173 @@ const LAYER_ID := 0
 const SOURCE_ID := 0
 
 
-const BLOCKS := {
+const ELEMENTS := {
+	"IMORTAL" : Vector2i(3, 0),
 	"Air": Vector2i(-1, -1),
-	"Sand": Vector2i(0,0),
+	"Sand": Vector2i(0, 0),
 	"Stone": Vector2i(1, 0),
+	"Whater": Vector2i(2, 0),
 }
-var selected_block := BLOCKS.Sand
+var selected_element := ELEMENTS.Sand
 
-var toch_press := false
-var toch_pos
+func _ready() -> void:
+	randomize()
+
+
+func sand_process() -> void:
+	var sands := get_used_cells_by_id(LAYER_ID, SOURCE_ID, ELEMENTS.Sand)
+	sands.sort_custom(func(a,b): return a.y > b.y)
+	for sand in sands:
+		var bottom_cell := sand + Vector2i.DOWN
+		var left_cell := sand + Vector2i.LEFT
+		var left_bottom_cell := left_cell + Vector2i.DOWN
+		var right_cell := sand + Vector2i.RIGHT
+		var right_bottom_cell = right_cell + Vector2i.DOWN
+		
+		
+		
+		if get_cell_atlas_coords(LAYER_ID, bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, sand)
+			set_cell(LAYER_ID, bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, left_bottom_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Air:
+			if randi_range(0, 1) == 0:
+				erase_cell(LAYER_ID, sand)
+				set_cell(LAYER_ID, left_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+			else:
+				erase_cell(LAYER_ID, sand)
+				set_cell(LAYER_ID, right_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, left_bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, sand)
+			set_cell(LAYER_ID, left_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+		
+		
+		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, sand)
+			set_cell(LAYER_ID, right_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+		
+		elif get_cell_atlas_coords(LAYER_ID, bottom_cell) == ELEMENTS.Whater:
+			erase_cell(LAYER_ID, sand)
+			erase_cell(LAYER_ID, bottom_cell)
+			set_cell(LAYER_ID, bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+			set_cell(LAYER_ID, sand, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Whater and get_cell_atlas_coords(LAYER_ID, left_bottom_cell) == ELEMENTS.Whater:
+			erase_cell(LAYER_ID, sand)
+			erase_cell(LAYER_ID, left_bottom_cell)
+			set_cell(LAYER_ID, left_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+			set_cell(LAYER_ID, sand, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Whater and get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Whater:
+			erase_cell(LAYER_ID, sand)
+			erase_cell(LAYER_ID, right_bottom_cell)
+			set_cell(LAYER_ID, right_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+			set_cell(LAYER_ID, sand, SOURCE_ID, ELEMENTS.Whater)
+
+
+func whater_process() -> void:
+	var whaters := get_used_cells_by_id(LAYER_ID, SOURCE_ID, ELEMENTS.Whater)
+	whaters.sort_custom(func(a,b): return a.y > b.y)
+	for whater in whaters:
+		var bottom_cell := whater + Vector2i.DOWN
+		var left_cell := whater + Vector2i.LEFT
+		var left_bottom_cell := left_cell + Vector2i.DOWN
+		var right_cell := whater + Vector2i.RIGHT
+		var right_bottom_cell = right_cell + Vector2i.DOWN
+		
+		
+		if get_cell_atlas_coords(LAYER_ID, bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, bottom_cell, SOURCE_ID, ELEMENTS.Whater)
+		
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, left_bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, left_bottom_cell, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, right_bottom_cell, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, left_cell, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, right_cell, SOURCE_ID, ELEMENTS.Whater)
+
 
 func _on_update() -> void:
-	sand_check()
+	queue_redraw()
+	sand_process()
+	whater_process()
 
-
-func sand_check() -> void:
-	for sand in get_used_cells_by_id(LAYER_ID, SOURCE_ID, BLOCKS.Sand):
-		if map_to_local(sand).y <= 477 and map_to_local(sand).x <= 510 and map_to_local(sand).x >= 0:
-			sand_process(sand)
-
-
-func sand_process(cord_in_world: Vector2i) -> void:
-	var bottom_neighbour := cord_in_world + Vector2i.DOWN
-	var left_bottom_neighbour := bottom_neighbour + Vector2i.LEFT
-	var right_bottom_neighbour := bottom_neighbour + Vector2i.RIGHT
-	
-	if get_cell_atlas_coords(LAYER_ID, bottom_neighbour) == BLOCKS.Air:
-		erase_cell(LAYER_ID, cord_in_world)
-		set_cell(LAYER_ID, bottom_neighbour, SOURCE_ID, BLOCKS.Sand)
-	
-	elif get_cell_atlas_coords(LAYER_ID, left_bottom_neighbour) == BLOCKS.Air:
-		erase_cell(LAYER_ID, cord_in_world)
-		set_cell(LAYER_ID, left_bottom_neighbour, SOURCE_ID, BLOCKS.Sand)
-	
-	elif get_cell_atlas_coords(LAYER_ID, right_bottom_neighbour) == BLOCKS.Air:
-		erase_cell(LAYER_ID, cord_in_world)
-		set_cell(LAYER_ID, right_bottom_neighbour, SOURCE_ID, BLOCKS.Sand)
-
-
-func set_block(pos) -> void:
-	if toch_press:
-		set_cell(LAYER_ID, local_to_map(pos), SOURCE_ID, selected_block)
-
-
+var is_touch := false
+var touch_position
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventScreenDrag:
-		if event.get_relative():
-			toch_press = true
-			toch_pos = event.position
-		else:
-			toch_press = false
-			toch_pos = event.position
 	if event is InputEventScreenTouch:
 		if event.is_pressed():
-			toch_press = true
-			toch_pos = event.position
-		else:
-			toch_press = false
-			toch_pos = event.position
-
+			is_touch = true
+			touch_position = event.position
+		else: 
+			is_touch = false
+	if event is InputEventScreenDrag:
+		if event.get_velocity() != Vector2.ZERO:
+			is_touch = true
+			touch_position = event.position
 
 func _process(_delta: float) -> void:
-	if toch_press:
-		set_block(toch_pos)
+	if is_touch:
+		set_block_on_mouse(touch_position)
+
+
+func set_block_on_mouse(toch_pos) -> void:
+	var mouse_pos_tile = local_to_map(toch_pos)
+	if selected_element != ELEMENTS.Air:
+		if get_cell_atlas_coords(LAYER_ID, mouse_pos_tile) == ELEMENTS.Air\
+		and get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.DOWN) == ELEMENTS.Air\
+		and get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.LEFT) == ELEMENTS.Air\
+		and get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.RIGHT) == ELEMENTS.Air\
+		and get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.UP) == ELEMENTS.Air:
+			set_cell(LAYER_ID, mouse_pos_tile, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.LEFT, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.RIGHT, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.UP, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.DOWN, SOURCE_ID, selected_element)
+	else:
+		if get_cell_atlas_coords(LAYER_ID, mouse_pos_tile) != ELEMENTS.IMORTAL and\
+		get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.LEFT) != ELEMENTS.IMORTAL and\
+		get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.RIGHT) != ELEMENTS.IMORTAL and\
+		get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.DOWN) != ELEMENTS.IMORTAL and\
+		get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.UP) != ELEMENTS.IMORTAL:
+			set_cell(LAYER_ID, mouse_pos_tile, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.LEFT, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.RIGHT, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.UP, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.DOWN, SOURCE_ID, selected_element)
 
 
 func _on_button_pressed() -> void:
-	match selected_block:
-		BLOCKS.Air:
-			selected_block = BLOCKS.Sand
-			$"../CanvasLayer/Control/Button".text = "SAND"
-		BLOCKS.Sand:
-			selected_block = BLOCKS.Stone
-			$"../CanvasLayer/Control/Button".text = "STONE"
-		BLOCKS.Stone:
-			selected_block = BLOCKS.Air
-			$"../CanvasLayer/Control/Button".text = "AIR"
+	match selected_element:
+		ELEMENTS.Air:
+			selected_element = ELEMENTS.Sand
+			$"../CanvasLayer/Control/VBoxContainer/Button".text = "SAND"
+		ELEMENTS.Sand:
+			selected_element = ELEMENTS.Stone
+			$"../CanvasLayer/Control/VBoxContainer/Button".text = "STONE"
+		ELEMENTS.Stone:
+			selected_element = ELEMENTS.Whater
+			$"../CanvasLayer/Control/VBoxContainer/Button".text = "WHATER"
+		ELEMENTS.Whater:
+			selected_element = ELEMENTS.Air
+			$"../CanvasLayer/Control/VBoxContainer/Button".text = "AIR"
--- simulation.gd
+++ simulation.gd
@@ -6,33 +6,175 @@ const SOURCE_ID := 0
 
 
 const ELEMENTS := {
+	"IMORTAL" : Vector2i(3, 0),
 	"Air": Vector2i(-1, -1),
 	"Sand": Vector2i(0, 0),
 	"Stone": Vector2i(1, 0),
+	"Whater": Vector2i(2, 0),
 }
+var selected_element := ELEMENTS.Sand
+
+func _ready() -> void:
+	randomize()
 
 
 func sand_process() -> void:
-	for sand in get_used_cells_by_id(LAYER_ID, SOURCE_ID, ELEMENTS.Sand):
+	var sands := get_used_cells_by_id(LAYER_ID, SOURCE_ID, ELEMENTS.Sand)
+	sands.sort_custom(func(a,b): return a.y > b.y)
+	for sand in sands:
 		var bottom_cell := sand + Vector2i.DOWN
 		var left_cell := sand + Vector2i.LEFT
 		var left_bottom_cell := left_cell + Vector2i.DOWN
 		var right_cell := sand + Vector2i.RIGHT
 		var right_bottom_cell = right_cell + Vector2i.DOWN
 		
+		
+		
 		if get_cell_atlas_coords(LAYER_ID, bottom_cell) == ELEMENTS.Air:
 			erase_cell(LAYER_ID, sand)
 			set_cell(LAYER_ID, bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, left_bottom_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Air:
+			if randi_range(0, 1) == 0:
+				erase_cell(LAYER_ID, sand)
+				set_cell(LAYER_ID, left_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+			else:
+				erase_cell(LAYER_ID, sand)
+				set_cell(LAYER_ID, right_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
 		
 		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Air and\
 		get_cell_atlas_coords(LAYER_ID, left_bottom_cell) == ELEMENTS.Air:
 			erase_cell(LAYER_ID, sand)
 			set_cell(LAYER_ID, left_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
 		
-		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air and get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Air:
+		
+		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Air:
 			erase_cell(LAYER_ID, sand)
 			set_cell(LAYER_ID, right_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+		
+		elif get_cell_atlas_coords(LAYER_ID, bottom_cell) == ELEMENTS.Whater:
+			erase_cell(LAYER_ID, sand)
+			erase_cell(LAYER_ID, bottom_cell)
+			set_cell(LAYER_ID, bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+			set_cell(LAYER_ID, sand, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Whater and get_cell_atlas_coords(LAYER_ID, left_bottom_cell) == ELEMENTS.Whater:
+			erase_cell(LAYER_ID, sand)
+			erase_cell(LAYER_ID, left_bottom_cell)
+			set_cell(LAYER_ID, left_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+			set_cell(LAYER_ID, sand, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Whater and get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Whater:
+			erase_cell(LAYER_ID, sand)
+			erase_cell(LAYER_ID, right_bottom_cell)
+			set_cell(LAYER_ID, right_bottom_cell, SOURCE_ID, ELEMENTS.Sand)
+			set_cell(LAYER_ID, sand, SOURCE_ID, ELEMENTS.Whater)
+
+
+func whater_process() -> void:
+	var whaters := get_used_cells_by_id(LAYER_ID, SOURCE_ID, ELEMENTS.Whater)
+	whaters.sort_custom(func(a,b): return a.y > b.y)
+	for whater in whaters:
+		var bottom_cell := whater + Vector2i.DOWN
+		var left_cell := whater + Vector2i.LEFT
+		var left_bottom_cell := left_cell + Vector2i.DOWN
+		var right_cell := whater + Vector2i.RIGHT
+		var right_bottom_cell = right_cell + Vector2i.DOWN
+		
+		
+		if get_cell_atlas_coords(LAYER_ID, bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, bottom_cell, SOURCE_ID, ELEMENTS.Whater)
+		
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, left_bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, left_bottom_cell, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air and\
+		get_cell_atlas_coords(LAYER_ID, right_bottom_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, right_bottom_cell, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, left_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, left_cell, SOURCE_ID, ELEMENTS.Whater)
+		
+		elif get_cell_atlas_coords(LAYER_ID, right_cell) == ELEMENTS.Air:
+			erase_cell(LAYER_ID, whater)
+			set_cell(LAYER_ID, right_cell, SOURCE_ID, ELEMENTS.Whater)
 
 
 func _on_update() -> void:
+	queue_redraw()
 	sand_process()
+	whater_process()
+
+var is_touch := false
+var touch_position
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			is_touch = true
+			touch_position = event.position
+		else: 
+			is_touch = false
+	if event is InputEventScreenDrag:
+		if event.get_velocity() != Vector2.ZERO:
+			is_touch = true
+			touch_position = event.position
+
+func _process(_delta: float) -> void:
+	if is_touch:
+		set_block_on_mouse(touch_position)
+
+
+func set_block_on_mouse(toch_pos) -> void:
+	var mouse_pos_tile = local_to_map(toch_pos)
+	if selected_element != ELEMENTS.Air:
+		if get_cell_atlas_coords(LAYER_ID, mouse_pos_tile) == ELEMENTS.Air\
+		and get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.DOWN) == ELEMENTS.Air\
+		and get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.LEFT) == ELEMENTS.Air\
+		and get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.RIGHT) == ELEMENTS.Air\
+		and get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.UP) == ELEMENTS.Air:
+			set_cell(LAYER_ID, mouse_pos_tile, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.LEFT, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.RIGHT, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.UP, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.DOWN, SOURCE_ID, selected_element)
+	else:
+		if get_cell_atlas_coords(LAYER_ID, mouse_pos_tile) != ELEMENTS.IMORTAL and\
+		get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.LEFT) != ELEMENTS.IMORTAL and\
+		get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.RIGHT) != ELEMENTS.IMORTAL and\
+		get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.DOWN) != ELEMENTS.IMORTAL and\
+		get_cell_atlas_coords(LAYER_ID, mouse_pos_tile + Vector2i.UP) != ELEMENTS.IMORTAL:
+			set_cell(LAYER_ID, mouse_pos_tile, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.LEFT, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.RIGHT, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.UP, SOURCE_ID, selected_element)
+			set_cell(LAYER_ID, mouse_pos_tile + Vector2i.DOWN, SOURCE_ID, selected_element)
+
+
+func _on_button_pressed() -> void:
+	match selected_element:
+		ELEMENTS.Air:
+			selected_element = ELEMENTS.Sand
+			$"../CanvasLayer/Control/VBoxContainer/Button".text = "SAND"
+		ELEMENTS.Sand:
+			selected_element = ELEMENTS.Stone
+			$"../CanvasLayer/Control/VBoxContainer/Button".text = "STONE"
+		ELEMENTS.Stone:
+			selected_element = ELEMENTS.Whater
+			$"../CanvasLayer/Control/VBoxContainer/Button".text = "WHATER"
+		ELEMENTS.Whater:
+			selected_element = ELEMENTS.Air
+			$"../CanvasLayer/Control/VBoxContainer/Button".text = "AIR"
+
+
+### AFK LOL
